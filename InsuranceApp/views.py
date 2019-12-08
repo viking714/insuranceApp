e94729769da6818b256ceeaa3d61d67b87c75e5c
viking714 <viking714@163.com>
2019-12-08 18:43:17 +0800

    version 1

--- InsuranceApp/views.py
+++ InsuranceApp/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template,jsonify
+from flask import render_template,jsonify,request,redirect,session
 from InsuranceApp import app
 
 
@@ -12,16 +12,49 @@ InsuranceDict = {1:{"name":"PingAn","age":"18-50","duration":"Whole Life","numbe
 				 2:{"name":"PingAnFu2019","age":"18-50","duration":"Whole Life","number":"100"}}
 
 
-@app.route('/')
+def loginCheck(func):
+	def wrapper(*args,**kwargs):
+		if session.get('user') != 'Joey' or session.get('pwd') != '123':
+			return render_template('login.html')
+		func(*args,**kwargs)
+	return wrapper
+
+
 @app.route('/home')
+@loginCheck
 def home():
     """Renders the home page."""
+
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
 		insurance = InsuranceDict
     )
+
+@app.route('/Detail/<int:nid>')
+def detail(nid):
+	info = InsuranceDict[nid]
+	return render_template(
+        'Detail.html',
+		insuranceDetail = info
+    )
+
+@app.route('/')
+@app.route('/login',methods=['GET','POST'])
+def login():
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		user = request.form.get('user')
+		password = request.form.get('password')
+		session['user'] = user
+		session['pwd'] = password
+		if user == 'Joey' and password == '123':
+			return render_template('index.html',title='Home Page',year=datetime.now().year,insurance = InsuranceDict)
+		return render_template('login.html',error='user or password error')
+
+
 
 @app.route('/contact')
 def contact():
